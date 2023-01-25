68f5eece5f60c1993d6b893a3c94246879b072dd
Jakob Rathberger <jakob.rathberger@gmail.com>
2023-01-25 11:43:14 +0100

    add employee class #5

---
+++ backend/App/Core/Workloads/Employees/Employee.cs
@@ -0,0 +1,7 @@
+namespace MongoDBDemoApp.Core.Workloads.Employees;
+
+public sealed class Employee
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+}
