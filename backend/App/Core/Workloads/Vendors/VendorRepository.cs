c5b7d8dad082c584462aca36d8c70a6b4c0bf978
Jakob Rathberger <jakob.rathberger@gmail.com>
2023-01-25 11:44:54 +0100

    add Vendor Repository #5

---
+++ backend/App/Core/Workloads/Vendors/VendorRepository.cs
@@ -0,0 +1,39 @@
+using LeoMongo;
+using LeoMongo.Database;
+using LeoMongo.Transaction;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDBDemoApp.Core.Workloads.Products;
+
+namespace MongoDBDemoApp.Core.Workloads.Vendor;
+
+public class VendorRepository: RepositoryBase<Vendor>, IVendorRepository
+{
+    
+    public VendorRepository(ITransactionProvider transactionProvider, IDatabaseProvider databaseProvider) : base(transactionProvider, databaseProvider)
+    {
+    }
+
+    public override string CollectionName { get; } = MongoUtil.GetCollectionName<Vendor>();
+
+    public Task<Vendor> AddVendor(Vendor vendor)
+    {
+        return InsertOneAsync(vendor);
+    }
+
+    public async Task<IReadOnlyCollection<Vendor>> GetAllVendors()
+    {
+        return await Query().ToListAsync();
+    }
+
+    public async Task<Vendor?> GetVendorById(ObjectId id)
+    {
+        return await Query().SingleOrDefaultAsync(v => v.Id == id);
+    }
+
+    public Task DeleteVendor(ObjectId id)
+    {
+        return DeleteOneAsync(id);
+    }
+}
