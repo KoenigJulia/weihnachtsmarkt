92ec6ccc15f3410d88eb673c61012c3ead7d1b86
Jakob Rathberger <jakob.rathberger@gmail.com>
2023-01-28 16:21:33 +0100

    add unique index for vendor name #5

--- backend/App/Core/Workloads/Vendors/VendorRepository.cs
+++ backend/App/Core/Workloads/Vendors/VendorRepository.cs
@@ -10,9 +10,14 @@ namespace MongoDBDemoApp.Core.Workloads.Vendors;
 
 public class VendorRepository: RepositoryBase<Vendor>, IVendorRepository
 {
+    private readonly IDatabaseProvider _databaseProvider;
+    
+    private IMongoCollection<TCollection> GetCollection<TCollection>(string collectionName) => this._databaseProvider.Database.GetCollection<TCollection>(collectionName);
     
     public VendorRepository(ITransactionProvider transactionProvider, IDatabaseProvider databaseProvider) : base(transactionProvider, databaseProvider)
     {
+        _databaseProvider = databaseProvider;
+        AddUniqueNameIndex();
     }
 
     public override string CollectionName { get; } = MongoUtil.GetCollectionName<Vendor>();
@@ -54,5 +59,17 @@ public class VendorRepository: RepositoryBase<Vendor>, IVendorRepository
     public async Task<List<Employee>> GetAllEmployees()
     {
         return await Query().Select(v => v.Employees).SelectMany(x => x).ToListAsync();
+    }
+
+    private async void AddUniqueNameIndex()
+    {
+        var indexOption = new CreateIndexOptions
+        {
+            Unique = true
+        };
+        var indexKeys = Builders<Vendor>.IndexKeys.Ascending(v => v.Name);
+        var indexModel = new CreateIndexModel<Vendor>(indexKeys, indexOption);
+        var col = GetCollection<Vendor>(CollectionName);
+        await col.Indexes.CreateOneAsync(indexModel);
     }
 }
