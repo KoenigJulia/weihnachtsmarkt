12d80a46eaa46b251942f2733cfe4bf54aefa390
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2023-01-29 18:08:27 +0100

    add TestAddProductToVendor test

---
+++ backend/Test/VendorServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using MongoDB.Bson;
+using MongoDBDemoApp.Core.Workloads.Places;
+using MongoDBDemoApp.Core.Workloads.Vendors;
+using NSubstitute;
+using Xunit;
+
+namespace MongoDBDemoApp.Test;
+
+public class VendorServiceTests
+{
+    [Fact]
+    public async Task TestAddProductToVendor()
+    {
+        var vendorId = new ObjectId();
+        var productId = new ObjectId();
+
+        var repoMock = Substitute.For<IVendorRepository>();
+        repoMock.AddProductToVendor(Arg.Is(productId), Arg.Is(vendorId)).Returns(true);
+
+        var service = new VendorService(repoMock, Substitute.For<IPlaceRepository>());
+        var result = await service.AddProductToVendor(productId, vendorId);
+
+        await repoMock.Received(1).AddProductToVendor(Arg.Is(productId), Arg.Is(vendorId));
+        result.Should().BeTrue();
+    }
+}
